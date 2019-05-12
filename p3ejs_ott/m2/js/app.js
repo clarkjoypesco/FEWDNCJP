af026ae5b22496dbfcf658c7e40600d9904cff95
Clark Joy Pesco <clarkjoypesco@yahoo.com>
2019-05-12 11:16:26 +0800

    Closure

--- p3ejs_ott/m2/js/app.js
+++ p3ejs_ott/m2/js/app.js
@@ -296,3 +296,27 @@ const results = musicData.filter(function(music) {
 });
 
 console.log(results);
+
+/*
+
+Declare a function named `expandArray()` that:
+
+* Takes no arguments
+* Contains a single local variable, `myArray`, which points to [1, 1, 1]
+* Returns an anonymous function that directly modifies `myArray` by
+  appending another `1` into it
+* The returned function then returns the value of `myArray`
+
+*/
+
+function expandArray() {
+  let myArray = [1, 1, 1];
+
+  return function() {
+    myArray.push(1);
+    return myArray;
+  };
+}
+const expand = expandArray();
+
+console.log(expand());
