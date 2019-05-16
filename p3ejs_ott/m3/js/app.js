6539e2efff7d4388a80ba340e4dac70b5d172580
Clark Joy Pesco <clarkjoypesco@yahoo.com>
2019-05-17 07:59:44 +0800

    Maps

--- p3ejs_ott/m3/js/app.js
+++ p3ejs_ott/m3/js/app.js
@@ -308,3 +308,175 @@ const myVehicle = new Vehicle();
 myVehicle.honkHorn(); // beep beep
 const myBike = new Bicycle();
 myBike.honkHorn(); // honk honk
+
+//NEW BUILT INS
+
+//Symbol
+
+const sym1 = Symbol("apple");
+console.log(sym1);
+
+const sym2 = Symbol("banana");
+const sym3 = Symbol("banana");
+console.log(sym2 === sym3);
+
+const bowl = {
+  [Symbol("apple")]: { color: "red", weight: 136.078 },
+  [Symbol("banana")]: { color: "yellow", weight: 183.15 },
+  [Symbol("orange")]: { color: "orange", weight: 170.097 },
+  [Symbol("banana")]: { color: "yellow", weight: 176.845 }
+};
+console.log(bowl);
+
+//Iterator Protocol
+const digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const arrayIterator = digits[Symbol.iterator]();
+
+console.log(arrayIterator.next());
+console.log(arrayIterator.next());
+console.log(arrayIterator.next());
+
+//Set
+
+// const games = new Set();
+// console.log(games);
+
+const games = new Set([
+  "Super Mario Bros.",
+  "Banjo-Kazooie",
+  "Mario Kart",
+  "Super Mario Bros."
+]);
+console.log(games);
+
+games.add("Banjo-Tooie");
+games.add("Age of Empires");
+games.delete("Super Mario Bros.");
+
+console.log(games);
+games.clear();
+console.log(games);
+
+const months = new Set([
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December"
+]);
+console.log(months.size);
+
+console.log(months.has("September"));
+
+console.log(months.values());
+
+const iterator = months.values();
+iterator.next();
+iterator.next();
+
+//Using For Of Loop in a Set
+const colors = new Set([
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "violet",
+  "brown",
+  "black"
+]);
+for (const color of colors) {
+  console.log(color);
+}
+
+/*
+ * Programming Quiz: Using Sets (3-1)
+ *
+ * Create a Set object and store it in a variable named `myFavoriteFlavors`. Add the following strings to the set:
+ *     - chocolate chip
+ *     - cookies and cream
+ *     - strawberry
+ *     - vanilla
+ *
+ * Then use the `.delete()` method to remove "strawberry" from the set.
+ */
+
+const myFavoriteFlavors = new Set([
+  "chocolate chip",
+  "cookies and cream",
+  "strawberry",
+  "vanilla"
+]);
+console.log(myFavoriteFlavors);
+console.log(myFavoriteFlavors.values());
+myFavoriteFlavors.delete("strawberry");
+console.log(myFavoriteFlavors.values());
+
+//WeakSet
+
+let student1 = { name: "James", age: 26, gender: "male" };
+let student2 = { name: "Julia", age: 27, gender: "female" };
+let student3 = { name: "Richard", age: 31, gender: "male" };
+
+const roster = new WeakSet([student1, student2, student3]);
+console.log(roster);
+student3 = null;
+
+console.log(roster);
+
+/*
+ * Programming Quiz: Using Sets (3-2)
+ *
+ * Create the following variables:
+ *     - uniqueFlavors and set it to a new WeakSet object
+ *     - flavor1 and set it equal to `{ flavor: 'chocolate' }`
+ *     - flavor2 and set it equal to an object with property 'flavor' and value of your choice!
+ *
+ * Use the `.add()` method to add the objects `flavor1` and `flavor2` to `uniqueFlavors`
+ * Use the `.add()` method to add the `flavor1` object (again!) to the `uniqueFlavors` set
+ */
+
+const uniqueFlavors = new WeakSet();
+let flavor1 = { flavor: "chocolate" };
+let flavor2 = { name: "Mango Float", flavor: "mango", price: 149 };
+
+uniqueFlavors.add(flavor1);
+uniqueFlavors.add(flavor2);
+uniqueFlavors.add(flavor1);
+console.log(uniqueFlavors);
+
+//Maps
+const employees = new Map();
+
+employees.set("james.parkes@udacity.com", {
+  firstName: "James",
+  lastName: "Parkes",
+  role: "Content Developer"
+});
+
+employees.set("julia@udacity.com", {
+  firstName: "Julia",
+  lastName: "Van Cleve",
+  role: "Content Developer"
+});
+employees.set("richard@udacity.com", {
+  firstName: "Richard",
+  lastName: "Kalehoff",
+  role: "Content Developer"
+});
+
+console.log(employees);
+
+employees.delete("julia@udacity.com");
+employees.delete("richard@udacity.com");
+console.log(employees);
+
+employees.clear();
+console.log(employees);
