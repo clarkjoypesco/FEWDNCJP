9ba0dd815f30c43b46a71e1088e6289d49d5534c
Clark Joy Pesco <clarkjoypesco@yahoo.com>
2019-05-22 19:12:06 +0800

    Built Ins Done

--- p3ejs_ott/m3/js/app.js
+++ p3ejs_ott/m3/js/app.js
@@ -620,3 +620,155 @@ const agent = new Proxy(richard, handler);
 agent.payRate = 1000; // set the actor's pay to $1,000
 agent.payRate; // $850 the actor's actual pay
 */
+
+// Proxies vs. ES5 Getter/Setter
+
+// var obj = {
+//   _age: 5,
+//   _height: 4,
+//   get age() {
+//       console.log(`getting the "age" property`);
+//       console.log(this._age);
+//   },
+//   get height() {
+//       console.log(`getting the "height" property`);
+//       console.log(this._height);
+//   }
+// };
+
+// obj.age; // logs 'getting the "age" property' & 5
+// obj.height; // logs 'getting the "height" property' & 4
+
+// obj.weight = 120; // set a new property on the object
+// obj.weight; // logs just 120
+
+const proxyObj = new Proxy(
+  { age: 5, height: 4 },
+  {
+    get(targetObj, property) {
+      console.log(`getting the ${property} property`);
+      console.log(targetObj[property]);
+    }
+  }
+);
+
+proxyObj.age; // logs 'getting the age property' & 5
+proxyObj.height; // logs 'getting the height property' & 4
+
+proxyObj.weight = 120; // set a new property on the object
+proxyObj.weight; // logs 'getting the weight property' & 120
+
+//Generator and Iterator
+//Pausable Functions
+
+// function* getEmployee() {
+//   console.log("the function has started");
+
+//   const names = [
+//     "Amanda",
+//     "Diego",
+//     "Farrin",
+//     "James",
+//     "Kagure",
+//     "Kavita",
+//     "Orit",
+//     "Richard"
+//   ];
+
+//   for (const name of names) {
+//     console.log(name);
+//     yield;
+//   }
+
+//   console.log("the function has ended");
+// }
+// const generatorIterator = getEmployee();
+// generatorIterator.next();
+
+// function* getEmployee() {
+//   console.log("the function has started");
+
+//   const names = [
+//     "Amanda",
+//     "Diego",
+//     "Farrin",
+//     "James",
+//     "Kagure",
+//     "Kavita",
+//     "Orit",
+//     "Richard"
+//   ];
+
+//   for (const name of names) {
+//     yield name;
+//   }
+
+//   console.log("the function has ended");
+// }
+
+// const generatorIterator = getEmployee();
+// let result = generatorIterator.next();
+// result.value; // is "Amanda"
+
+// generatorIterator.next().value; // is "Diego"
+// generatorIterator.next().value; // is "Farrin"
+
+//Sending Data into or out of generator
+
+// function* displayResponse() {
+//   const response = yield;
+//   console.log(`Your response is "${response}"!`);
+// }
+
+// const itrator = displayResponse();
+
+// itrator.next(); // starts running the generator function
+// itrator.next("Hello Udacity Student"); // send data into the generator
+// // the line above logs to the console: Your response is "Hello Udacity Student"!
+
+function* getEmployee() {
+  const names = [
+    "Amanda",
+    "Diego",
+    "Farrin",
+    "James",
+    "Kagure",
+    "Kavita",
+    "Orit",
+    "Richard"
+  ];
+  const facts = [];
+
+  for (const name of names) {
+    // yield *out* each name AND store the returned data into the facts array
+    facts.push(yield name);
+  }
+
+  return facts;
+}
+
+const generatorIterator = getEmployee();
+
+// get the first name out of the generator
+let name = generatorIterator.next().value;
+
+// pass data in *and* get the next name
+name = generatorIterator.next(`${name} is cool!`).value;
+
+// pass data in *and* get the next name
+name = generatorIterator.next(`${name} is awesome!`).value;
+
+// pass data in *and* get the next name
+name = generatorIterator.next(`${name} is stupendous!`).value;
+
+// you get the idea
+name = generatorIterator.next(`${name} is rad!`).value;
+name = generatorIterator.next(`${name} is impressive!`).value;
+name = generatorIterator.next(`${name} is stunning!`).value;
+name = generatorIterator.next(`${name} is awe-inspiring!`).value;
+
+// pass the last data in, generator ends and returns the array
+const positions = generatorIterator.next(`${name} is magnificent!`).value;
+
+// displays each name with description on its own line
+positions.join("\n");
