68c81b569e80b6196260299840cf10d7c6cba65e
Clark Joy Pesco <clarkjoypesco@yahoo.com>
2019-05-31 11:53:30 +0800

    Premium Cat Clicker

--- p4fe_app/01_cat_clicker/js/app.js
+++ p4fe_app/01_cat_clicker/js/app.js
@@ -1,42 +1,72 @@
 const data = [
-  {
-    name: "Angela"
-  },
-  {
-    name: "Cathy"
-  }
+  { id: 1, name: "Angela", image: "cat0.jpg", vote: 0 },
+  { id: 2, name: "Cathy", image: "cat1.jpg", vote: 0 },
+  { id: 3, name: "Rose", image: "cat2.jpg", vote: 0 },
+  { id: 4, name: "Karina", image: "cat3.jpg", vote: 0 },
+  { id: 5, name: "Brenda", image: "cat4.jpg", vote: 0 }
 ];
 
 //Listen for cancel
 document.querySelector("#content").addEventListener("click", catClick);
-
+document.querySelector("#nav-cats").addEventListener("click", catClickNav);
 const cats = data;
 let html = "";
-cats.forEach(function(cat, index) {
+cats.forEach(function(cat) {
   html += `
-            
-      <div class="col-md-6 mx-auto py-5 text-center p-2">
-      <div class="cat">
-        <h3 class="catsname">${cat.name}</h3>
-    
-        <div class="imgcat">
-          <img src="img/cat${index}.gif" alt="cat" class="rounded img-fluid" />
-        </div>
-    
-        <h4>0</h4>
-      </div>
-    </div>      
-            `;
+      <li class="nav-item">
+      <a class="nav-link" href="#">${cat.name} <span id="${cat.id}">0</span></a>
+    </li>
+    `;
 });
-document.getElementById("content").innerHTML = html;
-
+document.getElementById("nav-cats").innerHTML = html;
+findCat("Angela");
+function findCat(catName) {
+  cats.forEach(function(cat) {
+    if (cat.name === catName) {
+      html = `
+                
+            <div class="d-block mx-auto mb-4 py-5">
+            <div class="cat">
+              <h3 class="catsname">${cat.name}</h3>
+          
+              <div class="imgcat ">
+                <img src="img/${
+                  cat.image
+                }" alt="cat" class="rounded img-fluid" />
+              </div>
+          
+              <h4>${cat.vote}</h4>
+            </div>
+          </div>      
+                  `;
+    }
+  });
+  document.getElementById("content").innerHTML = html;
+}
+function voteCat(catName) {
+  cats.forEach(function(cat) {
+    if (cat.name === catName) {
+      cat.vote++;
+      document.getElementById(`${cat.id}`).textContent = cat.vote;
+    }
+  });
+}
 let count = 0;
 let currCount = 0;
 function catClick(e) {
   if (e.target.parentElement.classList.contains("imgcat")) {
     const counter = e.target.parentElement.nextElementSibling;
+    const name = e.target.parentElement.previousElementSibling.textContent;
     currCount = parseInt(counter.textContent) + 1;
     counter.textContent = currCount;
-    console.log(currCount);
+    voteCat(name);
+  }
+}
+
+function catClickNav(e) {
+  if (e.target.parentElement.classList.contains("nav-item")) {
+    let nameTextArr = e.target.textContent.split(" ");
+    let nameText = nameTextArr[0];
+    findCat(nameText);
   }
 }
