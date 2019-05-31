a429f565a1229e5b921604d404e4c9257547f61a
Clark Joy Pesco <clarkjoypesco@yahoo.com>
2019-05-31 09:33:22 +0800

    Dou Cat

--- p4fe_app/01_cat_clicker/js/app.js
+++ p4fe_app/01_cat_clicker/js/app.js
@@ -1,10 +1,42 @@
-//Event listener
-document.getElementById("imgcat").addEventListener("click", countClick);
+const data = [
+  {
+    name: "Angela"
+  },
+  {
+    name: "Cathy"
+  }
+];
 
-const counter = document.querySelector("#count");
+//Listen for cancel
+document.querySelector("#content").addEventListener("click", catClick);
 
-let i = 0;
-function countClick(e) {
-  i++;
-  counter.textContent = i;
+const cats = data;
+let html = "";
+cats.forEach(function(cat, index) {
+  html += `
+            
+      <div class="col-md-6 mx-auto py-5 text-center p-2">
+      <div class="cat">
+        <h3 class="catsname">${cat.name}</h3>
+    
+        <div class="imgcat">
+          <img src="img/cat${index}.gif" alt="cat" class="rounded img-fluid" />
+        </div>
+    
+        <h4>0</h4>
+      </div>
+    </div>      
+            `;
+});
+document.getElementById("content").innerHTML = html;
+
+let count = 0;
+let currCount = 0;
+function catClick(e) {
+  if (e.target.parentElement.classList.contains("imgcat")) {
+    const counter = e.target.parentElement.nextElementSibling;
+    currCount = parseInt(counter.textContent) + 1;
+    counter.textContent = currCount;
+    console.log(currCount);
+  }
 }
